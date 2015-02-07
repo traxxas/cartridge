3c049dce4b99d8c02368eba5f06dcb601c8364b4
Brandon Tuomela <brandon@jaanj.com>
2015-02-07 10:59:16 -0800

    add color slug prop for urls and variation segementing

--- cartridge/shop/models.py
+++ cartridge/shop/models.py
@@ -267,11 +267,15 @@ class ProductVariation(with_metaclass(ProductVariationMetaclass, Priced)):
             self.sku = self.id
             self.save()
 
+    def _color_slug(self):
+        return self.image.file.name[8:-9]
+    cslug = property(_color_slug)
+
 
     @models.permalink
     def get_absolute_url(self):
-        return ("specific_product", (), {"slug": self.product.slug,
-                                         "sku": self.sku})
+        return ("specific_product", (), {"slug": self.slug,
+                                         "cslug": self.cslug})
 
     @classmethod
     def option_fields(cls):
